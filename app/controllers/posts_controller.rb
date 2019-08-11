f415eb646acf3fc6067fa070bfef2992e2c93ca6
Jim <teachlife@outlook.com>
2019-08-10 20:57:55 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,15 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post.category = params[:category]
+    @post.content = params[:content]
+    @post.title = params[:title]
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
