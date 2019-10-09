5a88f6c9792505fdc5c5fe7dd650fad235c85d88
jgrantps <jgrantps@hotmail.com>
2019-10-09 10:54:53 -0300

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    #=> name cannot be blank
+    validates :name, :email, presence: true
+    #=> email has to be unique
+    validates :email, uniqueness: true
+ 
 end
