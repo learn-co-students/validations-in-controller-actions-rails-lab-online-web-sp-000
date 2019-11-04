0b605d6aadd01f26dd53fd4c84ebaa4f55059d0b
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-03 22:46:58 -0800

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,6 +8,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
+    #This ensures that the information written isn't lost. If I wanted to use just create method immidiately, the data would be lost, if params was not acceptable.
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
