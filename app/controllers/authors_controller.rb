a457dff4988d6b2c4a9396b0235db2aa8c2051c8
rohanphillips <rohan@solutions-insurance.com>
2019-11-12 19:09:48 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,22 @@
+require 'pry'
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
 
   def new
+    binding.pry
   end
 
-  def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+  def create   
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save     
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+    
   end
 
   private
---
+++ app/validators/category_validator.rb
@@ -0,0 +1,8 @@
+require 'pry'
+class CategoryValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.category.match?(/^Fiction/) || record.category.match?(/^Non-Fiction/)
+      record.errors[:category] << "Category must be either Fiction or Non-Fiction"
+    end
+  end
+end
