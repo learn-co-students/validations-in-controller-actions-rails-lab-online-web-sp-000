79b7d5884c4888c57013531ede05b9522d119341
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-08 16:09:20 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,7 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-
-  validates_with CategoryValidator
 
   validates :title, presence: true
-
   validates :content, length: { minimum: 100 }
-
-  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
 
 end
--- app/validators/category_validator.rb
+++
@@ -1,8 +0,0 @@
-class CategoryValidator < ActiveModel::Validator
-
-  def validate(record)
-    unless record.category.match?('Fiction' || 'Non-Fiction')
-      record.errors[:category] << "You are only allowed to specify 'Fiction' or 'Non-Fiction'"
-    end
-  end
-end
