fc033bf8693468c80086f1c386ec5f9dcba43794
Christopher Dent <christopherdent01@gmail.com>
2020-02-06 21:00:05 -0500

    Done.

---
+++ app/validators/CategoryValidator.rb
@@ -0,0 +1,7 @@
+#class CategoryValidator < ActiveModel::Validator
+#  def validate(record)
+#    unless record == "Fiction" || record == "Non-Fiction"
+#      record.errors[:category] << "is not a valid size!"
+#    end
+#  end
+#end
