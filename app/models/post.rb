10cd6356a10e94b0c2c313acb84349dbd9dd65d5
azbury <acasbury25@aol.com>
2020-02-27 20:03:40 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
-    include ActiveModel::Validations
-    validates_with CategoryValidator
+    validates :category, :exclusion => { :in => %w(Fiction Non-Fiction) }
     validates :title, presence: true
     validates :content, length: { minimum: 100 }
 end
