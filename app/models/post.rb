0fdda8e644cc67bd0347b4435d1ae74399be1dee
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-03-05 21:02:46 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates_with CategoryValidator, fields: [:category]
+  validates :content, length: {minimum: 100}
 end
