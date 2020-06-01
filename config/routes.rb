b05b67f4a02dcf0471dced4642750f0d4673da34
Alvin Coulson <aaacoulson@gmail.com>
2020-06-01 17:46:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :authors, only: [:index]
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
