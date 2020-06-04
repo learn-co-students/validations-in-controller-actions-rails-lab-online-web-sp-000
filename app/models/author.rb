e62d441b8b7215bd58ecb8518f95ccbe58efea7a
dkw5011 <dkw5011@gmail.com>
2020-06-04 19:40:05 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    validates :name, length: { minimum: 1 }
+    # validates :bio, length: { maximum: 500 }
+    # validates :password, length: { in: 6..20 }
+    # validates :registration_number, length: { is: 6 }
+    validates :email, uniqueness: true
 end
