6d1a7be6f158d0a5f822da1e41b790672ec9435b
ksummerill <kaitlynsummerill@gmail.com>
2020-07-01 07:40:23 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with 
+  validates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: { minimum: 100 }
 end
