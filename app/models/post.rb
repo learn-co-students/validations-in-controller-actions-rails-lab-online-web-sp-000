4bedc6a745919f68bfa8db3beba48734f2836a3f
Ronda Cowen <cowenhome5@gmail.com>
2020-07-19 12:13:17 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :content, presence: true, length: { minimum: 100 }
+    validates :title, presence: true
+        
+    validates_inclusion_of :category, :in => %w(fiction non-fiction)
+        
 end
