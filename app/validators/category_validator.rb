1112572590f284d1c63c8d819c0c1c22b1920d7f
yarvinh <yarvinhhernandez@gmail.com>
2020-09-25 17:55:25 -0400

    Done.

---
+++ app/validators/category_validator.rb
@@ -0,0 +1,11 @@
+
+# class CategoryValidator < ActiveModel::Validator
+    
+#     def validate(record)
+#             if record.category == "Fiction" || record.category == "Non-Fiction"
+#               record.errors[:base] << "TMust be either Fiction or Non-Fiction"      
+#             end
+#         end
+#   end
+
+ 
