47f424e0b50a0946b545e0406ef6bb8854a950c9
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-11 05:54:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: [minimum: 100]
-  validates :category, in: (Fiction || Non-Fiction)
+  validates :category, { in: (Fiction || Non-Fiction) }
 end
