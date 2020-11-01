bd2e4a01ab94a7d64a647b38b5769fae88c8e02c
khoinguyenkc <khoinguyenkc@gmail.com>
2020-10-31 21:15:35 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, length: {minimum: 1}
+    validates :email, uniqueness: true
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :title, length: {minimum: 1}
+    validates :content, length: {minimum: 100}
+    include ActiveModel::Validations
+    validates_with CategoryValidator
+
+
+
 end
