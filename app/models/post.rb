4bbc98c02ed5ab58b6562eb70482281f96dc6951
rmholsen <jaguar.kitty@gmail.com>
2021-02-02 20:51:47 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, :category, presence: true 
+    validates :category, format: { with: /\A(Fiction)\z|\A(Non-Fiction)\z/}
+    validates :content, length: {minimum: 100}
 end
+
+#REGEX THING
+#
