f0ded093869bde35c05b75e48d0fba4f78736a2b
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-04 20:48:06 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,25 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.valid?
     @post.update(post_params)
 
     redirect_to post_path(@post)
+    else 
+      render :edit
+    end 
   end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :new 
+    end     
+  end
+
 
   private
 
