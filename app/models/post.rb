6604cf40cfba28701d13033e4262fe42a271b996
bailaoye <jeffbai996@gmail.com>
2021-02-04 22:43:45 +0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+require 'pry'
+class PostValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.category == "Fiction" || record.category == "Non-Fiction"
+      record.errors[:category] << "Category needs to be Fiction or Non-Fiction"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with PostValidator
+  validates :content, length: {minimum: 100}
+  validates :title, presence: true
 end
