b9dd66061c9ad80d045a0342986761f049aeee1f
Benjamin Cheng <bencheng200@gmail.com>
2021-03-21 23:28:43 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.find_by(id: params[:id])
+    @post.assign_attributes(post_params)
+    if @post.valid? == true
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
