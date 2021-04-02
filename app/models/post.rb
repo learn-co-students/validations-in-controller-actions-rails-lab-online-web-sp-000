905d92714bf23c8ac65c40b087f5265cba6d6516
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-04-02 00:44:31 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, :category, :content, presence:true 
+  validates :title, :category, uniqueness: true 
+  validates :content, length: {minimum: 100}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
