4aca691b1388ed68334d185c5f7a551bfcdd055c
mttwright <mttwright@gmail.com>
2021-04-23 23:10:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    include ActiveModel::Validations
+    validates_with CategoryValidator
+    validates :content, length: { minimum: 100 }
 end
