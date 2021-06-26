16727f5d346a6227a9e836570cf88011b157236d
kiran ram <kram@lions.molloy.edu>
2021-06-26 22:03:02 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,26 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
+    
+    if @post.valid? 
+      
+      @post.save
 
     redirect_to post_path(@post)
+    
+  else
+    
+    render :new
+  end
+  end
+  
+  def update
+    @post.update(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
